cdd8edffa6a08154f976742f4065ef4f1993ad64
vojdel <vasquezpinedaj@gmail.com>
2020-11-15 03:33:29 -0400

    feat: add menu and section abilities

--- src/components/abilities.vue
+++ src/components/abilities.vue
@@ -1,14 +1,41 @@
 <template>
+  <section class="container p-4">
+    <div class="row">
+      <div class="col"><h4 class="text-center">Habilidades</h4></div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <div class="row" v-for="item in abilities" :key="item.id">
+          <strong class="col">{{ item.abilitie }}</strong><span class="col
+text-right">{{ item.value }}%</span><br>
+          <progress max="100" :value="item.value" class="w-100">{{
+item.value }}%</progress><br>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
   data() {
     return {
+      abilities: [
+        { abilitie: 'HTML5 | CSS3', value: 80 },
+        { abilitie: 'Javascript | Vuejs', value: 85 },
+        { abilitie: 'Nodejs', value: 75 },
+        { abilitie: 'PHP | Laravel', value: 80 },
+        { abilitie: 'Mysql | Postgresql', value: 65 },
+        { abilitie: 'Python', value: 30 },
+        { abilitie: 'Git | Github', value: 75 },
+      ]
     }
   }
 }
 </script>
 
-<style>
+<style scope>
+  progress{
+      height: 22.5px;
+  }
 </style>
--- src/components/menu.vue
+++ src/components/menu.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="#">Jose Daniel Vasquez Pineda</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse"
 data-target="#navbarSupportedContent"
 aria-controls="navbarSupportedContent" aria-expanded="false"
