45e5965d1931140d94dfdb3023c75f0a5065fbab
vojdel <vasquezpinedaj@gmail.com>
2020-11-14 21:51:17 -0400

    feat: add menu

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
+  <navegacion></navegacion>
   <h1>{{ tittle }}</h1>
   </div>
 </template>
 
 <script>
+import Menu from './components/menu'
 export default {
+  components: {
+    navegacion: Menu
+            },
   data() {
     return {
     tittle: 'Welcome my Personal Page!!'
