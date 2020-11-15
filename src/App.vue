00bde4530bb0ba833ab1820bd1b87d7b889a5683
vojdel <vasquezpinedaj@gmail.com>
2020-11-15 03:32:41 -0400

    feat: add section about me

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,26 @@
 <template>
   <div>
-  <navegacion></navegacion>
-  <h1>{{ tittle }}</h1>
+    <navegacion></navegacion>
+    <aboutMe></aboutMe>
+    <hr class="bg-seccondary w-100">
+    <abilities></abilities>
+    <hr class="bg-seccondary w-100">
+    <contact></contact>
   </div>
 </template>
 
 <script>
 import Menu from './components/menu'
+import AboutMe from './components/aboutMe'
+import Abilities from './components/abilities'
+import Contact from './components/contact'
+
 export default {
   components: {
-    navegacion: Menu
+    navegacion: Menu,
+    aboutMe: AboutMe,
+    abilities: Abilities,
+    contact: Contact
             },
   data() {
     return {
@@ -20,4 +31,7 @@ export default {
 </script>
 
 <style>
+  h4{
+      text-decoration: underline;
+  }
 </style>
--- src/components/aboutMe.vue
+++ src/components/aboutMe.vue
@@ -1,10 +1,47 @@
 <template>
+  <section class="container">
+    <div class="row mt-5">
+      <div class="col-6">
+        <h4 class="text-center">Sobre Mi</h4><br>
+        <p>Soy Desarrollador Web, soy amante de la musica, comics, mangas,
+de los libros en general y autodidactad en programación.</p>
+        <p>Me desempeño en las tecnologias como  javascript con Vue.js de
+Front-end y Node.js de back-end, pero tambien PHP y Laravel</p>
+      </div>
+      <div class="col-6">
+        <div class="card shadow p-3 mb-5 bg-white rounded">
+          <div class="card-body">
+            <p>
+            <strong class="text-dark">Fecha de Nacimiento</strong>: {{ fdn }} <br><br>
+            <strong>Lenguajes hablados</strong>: {{ allLenguages }}<br><br>
+            <strong>Nacionalidad</strong>: {{ nacionalidad }}<br><br>
+            <strong>Preferencias</strong>: {{ preferencs }}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
   data() {
     return {
+      fdn: "09-06-1999",
+      languages: [
+        'Español(materno)',
+        'Inglés(Intermedio)'
+             ],
+      nacionalidad: 'Venezolano',
+      preferencs: `Escuchar música (rock & metal) y Leer libros de ciencia
+        ficcion, y aventuras`
+    
+    }
+  },
+  computed: {
+    allLenguages: function(){
+      return `${this.languages[0]}, ${this.languages[1]}`
     }
   }
 }
